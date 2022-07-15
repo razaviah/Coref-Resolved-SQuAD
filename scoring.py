b9e8da75c3b718af2682a9985425bce3552f79f4
Amirhossein Razavi <108837052+razaviah@users.noreply.github.com>
2022-07-15 17:37:47 -0400

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -15,6 +15,8 @@ from nlgeval import NLGEval
 
 nlgeval = NLGEval()
 
+# Testing
+print("###############TEST###############")
 refs = [['what is perceived as the general priority of the food processing industry in this sort of economy ?'], ["what was kublai khan 's relation to ogedei khan ?"], ['in which two countries is the uk installing military bases ?'], ['when does byu refuse to play athletic games that got the attention of the sports networks ?', 'what violation can lead to a player being expelled from a sports team ?'], ['what do australian citizens need in order to travel to norfolk island ?'], ['carthage and tunisia are in what general area ?'], ['who proved that air is necessary for combustion ?', 'what researcher showed that air is a necessity for combustion ?', 'in what century did mayow and boyle perform their experiments ?'], ['why were laserdiscs more popular in japan ?']]
 hyps = ['in a profit-driven economy, health considerations are hardly a priority.', 'who offered kublai a position in xingzhou?', 'where is the uk establishing air and naval bases?', 'why has byu been criticized for refusing to play games on sunday?', 'what must australian citizens carry to travel to the island?', 'where is the bardo museum located?', 'who proved that air is necessary for combustion in the late 17th century?', 'why was laserdisc more popular in japan than north america?']
 print('len refs sample: ', len(refs))
@@ -22,6 +24,8 @@ print('len hyps sample: ', len(hyps))
 metrics_dict_sample = nlgeval.compute_metrics(refs, hyps)
 
 print(metrics_dict_sample)
+      
+print("###############DONE###############")
 
 
 CRSQuAD_Q = open('CRSQuAD_Q_lists.txt', 'r')
